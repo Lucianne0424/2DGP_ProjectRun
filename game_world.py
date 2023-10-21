cb779fa700415757f423387145285ea7a8ea5d88
Lucianne0424 <pwj0424@naver.com>
2023-10-21 23:14:45 +0900

    ProjectRun(가제) 뼈대 제작

---
+++ game_world.py
@@ -0,0 +1,33 @@
+# 게임 월드 모듈
+
+world = [[], [], []]  # 레이어 구별
+
+
+# 게임 월드에 객체 담기
+
+def add_object(o, depth=0):
+    world[depth].append(o)
+
+
+# 게임 월드 객체들을 모두 업데이트
+def updata():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+
+# 게임 월드 객체들을 모두 그리기
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
+
+
+# 객체 삭제
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+
+    raise ValueError('Remove Error')

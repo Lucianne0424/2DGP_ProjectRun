f7d8c7cb6cb7ada0401cdb880daac503fc3d901d
Lucianne0424 <pwj0424@naver.com>
2023-10-30 00:45:12 +0900

    배경 출력 클래스 이미지 변수 생성자-> 클래스 변수로 변경

--- background.py
+++ background.py
@@ -3,16 +3,17 @@ from image_load import image_load
 
 
 class BackGround:
+    image = None
     def __init__(self, x):
         self.x = x
-        self.image = image_load('.//img//BackGround', 'testBG.png')
-
+        if BackGround.image == None:
+            BackGround.image = image_load('.//img//BackGround', 'testBG.png')
 
     def update(self):
         self.x -= 10
-        if self.x <= -canvasSIZE[0]:
+        if self.x < -canvasSIZE[0]:
             remove_object(self)
-            add_object(BackGround(canvasSIZE[0]),0)
+            add_object(BackGround(canvasSIZE[0]) + 10,0)
 
 
     def draw(self):

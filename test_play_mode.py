d8dc5539e04d9ec0d9445e24308139bda2531ca8
Lucianne0424 <pwj0424@naver.com>
2023-11-06 21:00:59 +0900

    게임 프레임워크로 구조 변경

--- test_play_mode.py
+++ test_play_mode.py
@@ -1,4 +1,6 @@
 from pico2d import *
+
+import game_framework
 import game_world
 from background import BackGround
 from object_information import setting_stage
@@ -7,22 +9,18 @@ from point_object import PointObject, add_point_object
 
 
 def handle_events():
-    global running
-
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            running = False
+            game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+            game_framework.quit()
         else:
              player.handle_event(event)
 
-def create_world():
-    global running
+def init():
     global player
 
-    running = True
     player = Player()
     game_world.add_object(player, 1)
 
@@ -31,22 +29,19 @@ def create_world():
 
 
 
-def update_world():
+def update():
     game_world.updata()
     add_point_object()
 
-def render_world():
+def draw():
     clear_canvas()
     game_world.render()
     update_canvas()
 
-open_canvas(game_world.canvasSIZE[0], game_world.canvasSIZE[1])
-create_world()
-# game loop
-while running:
-    handle_events()
-    update_world()
-    render_world()
-    delay(0.1)
-# finalization code
-close_canvas()
+def finish():
+    game_world.clear()
+    pass
+
+def pause(): pass
+
+def resume(): pass

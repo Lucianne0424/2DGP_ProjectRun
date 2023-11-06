967a271405b906a9c743575922f0358bd2b4cd8d
Lucianne0424 <pwj0424@naver.com>
2023-11-06 22:10:33 +0900

    점수 오브젝트 프레임타임 적용

--- point_object.py
+++ point_object.py
@@ -1,3 +1,4 @@
+import game_framework
 from game_world import canvasSIZE, remove_object, add_object
 from image_load import image_load
 from object_information import setting_stage
@@ -9,11 +10,20 @@ point_object_load_count = 0 # 점수 오브젝트의 배치 정보를 저장한 
 point_object_information = setting_stage()
 point_object_information_len = len(point_object_information)
 
+
+PIXEL_PER_METER = (10.0 / 0.3)
+P_O_SPEED_KMPH = 30.0
+P_O_SPEED_MPM = (P_O_SPEED_KMPH * 1000.0 / 60.0)
+P_O_SPEED_MPS = (P_O_SPEED_MPM / 60.0)
+P_O_SPEED_PPS = (P_O_SPEED_MPS * PIXEL_PER_METER)
+
+
 def add_point_object(): # 일정 간격으로 점수 오브젝트 출력
     global PO_gap_count
     global point_object_load_count
-    PO_gap_count = (PO_gap_count + 1) % 5
-    if PO_gap_count == 5 - 1:
+    PO_gap_count = (PO_gap_count + 1.0 * game_framework.frame_time)
+    if PO_gap_count >= 0.2: # 1초에 점수 오브젝트 5개 생성
+        PO_gap_count = 0
         add_object(PointObject(point_object_information[point_object_load_count]), 2)
         point_object_load_count = (point_object_load_count + 1) % point_object_information_len
 
@@ -32,7 +42,7 @@ class PointObject:
             PointObject.image = image_load('.//img//Point', point_object_level_image_load())
 
     def update(self):
-        self.x -= 10
+        self.x -= P_O_SPEED_PPS * game_framework.frame_time
         if self.x <= 0 - 30:
             remove_object(self)
 

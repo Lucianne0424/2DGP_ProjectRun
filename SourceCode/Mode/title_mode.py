99c561352c88d5ceac18b03890c46c2f942eb022
Lucianne0424 <pwj0424@naver.com>
2023-12-04 02:00:31 +0900

    타이틀 모드에 버튼 오브젝트 추가 ( 이전에 사용하던 스페이스바, e 키 대신 버튼 오브젝트로 대체 )

--- SourceCode/Mode/title_mode.py
+++ SourceCode/Mode/title_mode.py
@@ -1,18 +1,26 @@
 from pico2d import get_events, clear_canvas, update_canvas, load_image
-from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDLK_e
+from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDLK_e, SDL_MOUSEMOTION, SDL_MOUSEBUTTONDOWN, \
+    SDL_BUTTON_LEFT
 
-from SourceCode.Etc import game_framework
-from SourceCode.Etc.global_variable import canvasSIZE
+from SourceCode.Etc import game_framework, mouse_event, game_world
+from SourceCode.Etc.global_variable import canvasSIZE, depth
 from SourceCode.Mode import test_play_mode, editor_mode
+from SourceCode.Object.button_object import ButtonObject
 
 
 def init():
     global image
     image = load_image('img/Title//title.png')
+    size_x = (canvasSIZE[0] / 2) + 500
+    size_y = (canvasSIZE[1] / 2) - 100
+
+    game_world.add_object(ButtonObject(size_x, size_y, 'play', '플레이', 45), depth['Button'])
+    game_world.add_object(ButtonObject(size_x, size_y - 100, 'editor', '에디터', 45), depth['Button'])
+    game_world.add_object(ButtonObject(size_x, size_y - 200, 'quit', '종   료', 45), depth['Button'])
 
 
 def finish():
-    pass
+    game_world.clear()
 
 
 def handle_events():
@@ -22,10 +30,18 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
-            game_framework.change_mode(test_play_mode)
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_e:
-            game_framework.change_mode(editor_mode)
+        elif event.type == SDL_MOUSEMOTION:
+            mouse_event.MousePos_x, mouse_event.MousePos_y = mouse_event.Mouse_event.return_mouse_pos(event)
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                t = mouse_event.Mouse_event.collision_Ui_object()
+                if t != False:
+                    if t.command == 'play':
+                        game_framework.change_mode(test_play_mode)
+                    elif t.command == 'editor':
+                        game_framework.change_mode(editor_mode)
+                    elif t.command == 'quit':
+                        game_framework.quit()
 
 
 def update():
@@ -35,6 +51,7 @@ def update():
 def draw():
     clear_canvas()
     image.draw_to_origin(0, 0, canvasSIZE[0], canvasSIZE[1])
+    game_world.render()
     update_canvas()
 
 
